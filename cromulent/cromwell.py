68c4705394a7e6c1199f69467a59b0f319296a1a
indraniel <indraniel@gmail.com>
2018-10-29 11:11:19 -0500

    + remove unused classes
      - nothing is using the Metadata and Execution classes at the moment

--- cromulent/cromwell.py
+++ cromulent/cromwell.py
@@ -7,36 +7,6 @@ from gcloud import GenomicsOperation
 
 import requests
 
-
-class Execution(object):
-
-    def __init__(self, json):
-        self.json = json
-
-    def status(self):
-        return self.json['executionStatus']
-
-    def shard(self):
-        return self.json['shardIndex']
-
-    def jobid(self):
-        return self.json.get('jobId', None)
-
-    def __str__(self):
-        return json.dumps(self.json, sort_keys=True, indent=4, separators=(',', ': '))
-
-
-class Metadata(object):
-
-    def __init__(self, metadata):
-        self.json_doc = metadata
-
-    def calls(self):
-        return {
-            k: [Execution(x) for x in v]
-            for k, v in self.json_doc['calls'].iteritems()
-            }
-
 class Server(object):
 
     def __init__(self, host="localhost", port=8000):
