f6db31d28957735cbecbaa1c4214e24917d851d9
Eddie Belter <ebelter@gmail.com>
2019-03-29 17:29:41 -0500

    Cromulent App
    connect to mysql
    close connection on destruction

--- cromulent/app.py
+++ cromulent/app.py
@@ -1,5 +1,6 @@
 # -- CromulentApp
 
+import pymysql.cursors
 from pyhocon import ConfigFactory
 import logging, os
 
@@ -12,5 +13,35 @@ class CromulentApp(object):
             logging.getLogger('root').info('Using config at {0}'.format(config_fname))
             self.config = ConfigFactory.parse_file(config_fname)
 
+    # -- __init
+
+    def __del__(self):
+        if self.db is not None:
+            self.db.close
+
+    # -- __del__
+
+    def connect(self):
+        if self.db is not None: return self.db
+
+        host = self.config.get("database.db.host", "localhost")
+        port = self.config.get("database.db.port", "3306")
+        url = self.config.get("database.db.url")
+        if url is not None:
+            #"jdbc:mysql://cromwell-mysql:3306/cromwell?rewriteBatchedStatements=true&useSSL=false"
+            (host, port) = url.split("/")[2].split(":")
+
+        self.db = pymysql.connect(
+            host=host,
+            port=port,
+            user=self.config.get("database.db.user", "root"),
+            password=self.config.get("database.db.password"), # only thing without a default
+            db=self.config.get("database.db.password", "cromwell"),
+            charset="utf8mb4",
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+        return self.db
+
+    # -- connect
 
 # -- CromulentApp (end)
