550ebc23e9ceea2f2278209fdba502a3095838ed
Eddie Belter <ebelter@gmail.com>
2019-04-01 14:59:56 -0500

    Test config attributes

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,8 @@ class CromulentAppTest(unittest.TestCase):
     def test(self):
         theapp = app.CromulentApp("tests/data/cromulent/app/jes.conf")
         self.assertIsNotNone(theapp)
-
+        self.assertEqual(theapp.config['database']['db']['user'], 'cromwell')
+        self.assertEqual(theapp.config['database']['db']['password'], 'words')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
